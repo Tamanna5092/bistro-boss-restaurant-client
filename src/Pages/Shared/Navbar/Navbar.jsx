0705dd341c5639f5177cfbdb526561c41732129e
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2025-05-15 09:23:28 +0600

    first commit

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navOptions = (
+    <>
+      <li className="uppercase font-medium"><Link to={"/"}>Home</Link></li>
+      <li className="uppercase font-medium"><Link to={""}>Dashboard</Link></li>
+      <li className="uppercase font-medium"><Link to={""}>Our menu</Link></li>
+      <li className="uppercase font-medium"><Link to={""}>Our shop</Link></li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {navOptions}
+          </ul>
+        </div>
+        <div className="flex flex-col">
+          <a className="text-base font-bold uppercase md:text-xl">Bistro boss</a>
+        <span className="font-medium tracking-[0.02rem] uppercase md:tracking-[0.2rem]">Restaurant</span>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+           {navOptions}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
